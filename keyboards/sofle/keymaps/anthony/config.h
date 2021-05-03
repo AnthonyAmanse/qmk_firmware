9b3ef8ec2ae7b77313bd09530898e42ac249ebc7
Anthony Amanse <ghieamanse@gmail.com>
2021-05-02 18:42:05 -0700

    Initial sofle keymap
    Signed-off-by: Anthony Amanse <ghieamanse@gmail.com>

---
+++ keyboards/sofle/keymaps/anthony/config.h
@@ -0,0 +1,6 @@
+#pragma once
+#define FORCE_NKRO
+/* The way how "handedness" is decided (which half is which),
+see https://docs.qmk.fm/#/feature_split_keyboard?id=setting-handedness
+for more options.
+*/
